5bcbc28fcba71fb4dc87b07350f130b844ee6ff4
take.y <yuuki.bado1002@gmail.com>
2023-02-23 09:50:10 +0900

    cleared station3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import { Router } from './routes/Router';
+
+import React from "react";
+import "./App.css";
+import { Router } from "./routes/Router";
 
 function App() {
   return (
     <div className="App">
-        <Router />
+      <Router />
     </div>
   );
 }
